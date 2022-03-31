59a0fc750c35c91573b0224827ca7ccd338abadc
Adrián Rey Lomba <adrianreylomba@gmail.com>
2022-03-31 12:31:30 +0200

    Minor design tweaks
    Co-authored-by: Angel Amado <angel-amado@users.noreply.github.com>

--- components/SectionAbout.tsx
+++ components/SectionAbout.tsx
@@ -3,10 +3,27 @@ import { useTranslation } from "next-i18next";
 export default function SectionAbout() {
   const { t } = useTranslation("common");
   return (
-    <section id="about">
-      <h2 className="font-bold text-cyan-400 text-xl text-center">
-        {t("section.about")}
-      </h2>
-    </section>
+    <div className="my-8">
+      <div className="flex items-center -skew-x-12">
+        <div className="-ml-6 h-10 w-1/3 bg-slate-200"></div>
+        <h2 className="ml-2 text-4xl font-extrabold uppercase">
+          {t("section.about")}
+        </h2>
+      </div>
+      <section id="about" className="bg-glass max-w-6xl mx-auto p-3 my-4">
+        <p className="italic">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          reiciendis nostrum modi repellendus totam inventore incidunt eveniet
+          veritatis dignissimos magni. Iusto labore dolor rerum omnis incidunt
+          aliquam similique eum beatae! Itaque accusantium magnam harum vitae
+          veritatis ad nam dignissimos odio quaerat, totam quis ipsam at illum
+          impedit quam nemo, in eaque magni voluptas voluptatum rerum amet
+          consequuntur! Alias, quod delectus. Consequuntur, quae repellat
+          accusantium harum animi quibusdam! Eos alias delectus aperiam aliquid
+          commodi, sunt eligendi itaque obcaecati dolorum consequatur inventore
+          eius ex quas, suscipit excepturi ipsam quis! Quidem, modi optio.
+        </p>
+      </section>
+    </div>
   );
 }
--- components/SectionTeam.tsx
+++ components/SectionTeam.tsx
@@ -8,32 +8,40 @@ export default function SectionTeam() {
   const { t } = useTranslation("common");
   const [activeProfile, setActiveProfile]: any = useState({});
   return (
-    <>
-      <h2 className="font-bold text-cyan-400 text-xl text-center">
-        {t("section.team")}
-      </h2>
-      <section
-        id="team"
-        className="bg-glass flex flex-wrap items-center justify-center p-8 gap-20"
-      >
-        <article className="flex flex-wrap justify-center max-w-md gap-3">
-          {profilesData.map((profile) => (
-            <Avatar
-              key={profile.id}
-              isActive={profile.id !== activeProfile.id}
-              profile={profile}
-              onClick={() => setActiveProfile(profile)}
-              onLoad={() =>
-                setActiveProfile(
-                  profilesData[Math.floor(Math.random() * profilesData.length)]
-                )
-              }
-            />
-          ))}
-        </article>
+    <div className="my-8">
+      <div className="flex items-center -skew-x-12">
+        <div className="-ml-6 h-10 w-1/3 bg-slate-200"></div>
+        <h2 className="ml-2 text-4xl font-extrabold uppercase">
+          {t("section.team")}
+        </h2>
+      </div>
 
-        <Card profile={activeProfile} />
-      </section>
-    </>
+      <div className="bg-glass max-w-6xl mx-auto p-3 my-4">
+        <section
+          id="team"
+          className="flex flex-wrap items-center justify-around p-8 gap-6 lg:gap-0"
+        >
+          <article className="flex flex-wrap justify-center max-w-md gap-3">
+            {profilesData.map((profile) => (
+              <Avatar
+                key={profile.id}
+                isActive={profile.id !== activeProfile.id}
+                profile={profile}
+                onClick={() => setActiveProfile(profile)}
+                onLoad={() =>
+                  setActiveProfile(
+                    profilesData[
+                      Math.floor(Math.random() * profilesData.length)
+                    ]
+                  )
+                }
+              />
+            ))}
+          </article>
+
+          <Card profile={activeProfile} />
+        </section>
+      </div>
+    </div>
   );
 }
