e3403e8269910b78e0a7b9d60c900fd672de2abb
Adrián Rey Lomba <adrianreylomba@gmail.com>
2022-04-22 16:16:20 +0200

    Fix NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -20,24 +20,28 @@ export default function NavBar() {
           <h1 className="ml-2 text-4xl">VALTUA</h1>
         </div>
 
-        <div className={isNavOpen ? "hidden md:block" : "flex"}>
-          <div className="w-full md:text-center text-left md:w-1/5">
+        <div
+          className={
+            isNavOpen ? "ml-2 flex flex-col md:flex-row" : "hidden md:flex"
+          }
+        >
+          <div className="w-full self-center text-left md:w-1/5 md:text-center">
             <a href="#about">{t("section.about")}</a>
           </div>
 
-          <div className="w-full md:text-center text-left md:w-1/5">
+          <div className="w-full self-center text-left md:w-1/5 md:text-center">
             <a href="#projects">{t("section.projects")}</a>
           </div>
 
-          <div className="hidden w-full md:text-center text-left md:block md:w-1/5">
-            <h1 className="text-4xl ">VALTUA</h1>
+          <div className="hidden w-full self-center text-left md:block md:w-1/5 md:text-center">
+            <h1 className="text-4xl">VALTUA</h1>
           </div>
 
-          <div className="w-full md:text-center text-left md:w-1/5">
+          <div className="w-full self-center text-left md:w-1/5 md:text-center">
             <a href="#team">{t("section.team")}</a>
           </div>
 
-          <div className="w-full md:text-center text-left md:w-1/5">
+          <div className="w-full self-center text-left md:w-1/5 md:text-center">
             <a href="#contact">{t("section.contact")}</a>
           </div>
         </div>
